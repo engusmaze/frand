7a13955f102326c0a338f2b575f73d338fdc135e
Gord Lea <jgordonlea@gmail.com>
2024-05-15 09:19:34 -0700

    feat: new features: thread_rng, impl_rng_core
    thread_rng adds support for thread local random number generation.
    
    impl_rng_core implements the RngCore and SeedableRng traits from rand.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,69 @@
+/*!
+**FRand** is a blazingly fast, small, and simple pseudo-random number 
+generator (PRNG) written in Rust. The advantage of using FRand is that 
+it can produce more random numbers per second than other libraries. It 
+also produces high-quality random numbers using a fast 
+**non-cryptographic** hashing algorithm.
+
+# Usage
+
+This crate is [on crates.io](https://crates.io/crates/regex) and can be
+used by adding `regex` to your dependencies in your project's `Cargo.toml`.
+
+```toml
+[dependencies]
+frand = "0.8"
+```
+
+# Example
+
+**FRand** is really simple to use. Here is a simple example of how to 
+use FRand to generate a random float:
+
+```rust
+use frand::Rand;
+
+let mut rng = Rand::new();
+println!("{}", rng.gen::<f32>());
+```
+
+# Crate features
+
+* **std** -
+    (default) Enables the use of the standard library. This feature is 
+    required for the `new` and `rehash` functions.
+* **alloc* -
+    (default) Enables the use of the `alloc` crate. This feature is 
+    required to allow shuffling of Boxes and Vecs.
+* **impl_rng_core** -
+    (default) Enables the implementation of the `rand::RngCore` and 
+    `rand::SeedableRng` traits for the `Rand` struct. This feature is
+    required to use FRand with the `rand` and things like `rand::distributions`.
+* **thread_rng** -
+    (default) Enables the thread local version of FRand. This can be
+    accessed using the `thread_frand` function.
+* **glam** -
+    (default) Uses glam to enable the generation of random values 
+    for glam::Vec2, glam::Vec3, glam::Vec3A, and glam::Vec4.
+
+
+ */
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use core::{mem::transmute, ops::Range};
 
+#[cfg(feature = "impl_rng_core")]
+use rand_core::impls::fill_bytes_via_next;
+#[cfg(feature = "impl_rng_core")]
+pub use rand::*;
+
 mod gen;
 pub use gen::*;
+
+#[cfg(feature = "thread_rng")]
+mod thread;
+#[cfg(feature = "thread_rng")]
+pub use thread::*;
 
 mod shuffle;
 pub use shuffle::*;
@@ -56,5 +116,38 @@ impl Rand {
     #[inline]
     pub fn rehash(&mut self) {
         *self = Self::new();
+    }
+}
+
+
+impl RngCore for Rand {
+    fn next_u32(&mut self) -> u32 {
+        self.gen::<u32>()
+    }
+
+    fn next_u64(&mut self) -> u64 {
+        self.gen::<u64>()
+    }
+    
+    fn fill_bytes(&mut self, dest: &mut [u8]) {
+        fill_bytes_via_next(self, dest);
+    }
+    
+    fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), rand::Error> {
+        self.fill_bytes(dest);
+        Ok(())
+    }
+}
+
+#[cfg(feature = "impl_rng_core")]
+impl SeedableRng for Rand {
+    type Seed = [u8; 8];
+
+    fn from_seed(seed: Self::Seed) -> Self {
+        Self::with_seed(u64::from_be_bytes(seed))
+    }
+
+    fn seed_from_u64(state: u64) -> Self {
+        Self::with_seed(state)
     }
 }
